ab8f1ff567acab28a8c7b5aeb6364a9d81e5f2af
Michael Wieland <michi.wieland@hotmail.com>
2016-10-24 15:02:45 +0200

    card view added

--- sunriseclock/app/src/main/java/ch/hsr/sunriseclock/sunriseclock/AlarmViewHolder.java
+++ sunriseclock/app/src/main/java/ch/hsr/sunriseclock/sunriseclock/AlarmViewHolder.java
@@ -4,14 +4,11 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
 
-import ch.hsr.sunriseclock.sunriseclock.domain.Alarm;
-
 
 public class AlarmViewHolder extends RecyclerView.ViewHolder {
 
     private View parent;
     private TextView textView;
-    private Alarm alarm;
 
     public AlarmViewHolder(View parent, TextView textView) {
         super(parent);
